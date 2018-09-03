c3329cc6c734eb1cc8b72560f234259b4e975fce
Herve Ekem <ekemherve@gmail.com>
2018-09-03 10:20:02 +0200

    Ajout component categorie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PageAccueilComponent } from './page-accueil/page-accueil.component';
 import { HeaderComponent } from './header/header.component';
+import { CategorieComponent } from './categorie/categorie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageAccueilComponent,
-    HeaderComponent
+    HeaderComponent,
+    CategorieComponent
   ],
   imports: [
     BrowserModule
