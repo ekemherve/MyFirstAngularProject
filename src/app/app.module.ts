40e5aa09bd99b56cc158ea62e8e4fe9b68a9fee9
Herve Ekem <ekemherve@gmail.com>
2018-09-03 10:03:02 +0200

    Ajout component page-accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageAccueilComponent } from './page-accueil/page-accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageAccueilComponent
   ],
   imports: [
     BrowserModule
